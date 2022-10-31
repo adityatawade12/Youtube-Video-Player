dc4f8af9d43614c8c7024b204463f3087a784fba
Aditya <aditya.tawade@somaiya.edu>
2022-10-31 18:49:17 +0530

    added youtube api

--- src/App.js
+++ src/App.js
@@ -2,10 +2,23 @@ import './App.css';
 
 import React, { Component } from 'react'
 import SearchBar from './components/SearchBar';
+import youtube from './apis/youtube';
+
 
 export default class App extends Component {
-  submitData=(e)=>{
-    console.log("bye")
+
+  state={videos:[]}
+  submitData=async (term)=>{
+    console.log(term)
+   const response= await youtube.get('/search',{
+      params:{
+        q:term
+      }
+
+    
+    })
+    this.setState({videos:response.data.items})
+    console.log(this.state.videos)
   }
   render() {
     return (
@@ -17,3 +30,5 @@ export default class App extends Component {
     );
   }
 }
+
+//AIzaSyA6ALaEf8xi4cl9_MJnFbkknEb9VCPnRzU
